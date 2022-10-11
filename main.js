e4045693f4a74361fdc3a1f33492769eba62d3f2
Juliana Akemy <akemyjuliana@gmail.com>
2022-10-11 20:29:50 -0300

    Alterações pedidas pelo tutor

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
             nome: {
                 required: true
             },
+            telefone: {
+                required: true
+            },
             email: {
                 required: true
             },
@@ -31,10 +34,13 @@ $(document).ready(function() {
         }
     })
 
-    $('#cpf').mask('00.000.000-00', {
-        placeholder: '00.000.000-00'
+    $('#cpf').mask('000.000.000-00', {
+        placeholder: '000.000.000-00'
     })
     $('#cep').mask('00000-000', {
         placeholder: '00000-000'
     })
-})
+
+    $('#telefone').mask('(00) 00000-0000', { 
+        placeholder: '(00) 00000-0000'})
+    })
